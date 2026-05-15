e95a4dd0397daa8e7c4949bedda0e5926f3e9938
baskanohn5 <baskanohn5@gmail.com>
2026-05-15 17:12:29 +0300

    Render firebase env destegi eklendi

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,12 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebaseServiceAccount.json");
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require("./firebaseServiceAccount.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
