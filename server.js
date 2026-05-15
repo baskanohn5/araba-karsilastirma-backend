7e369df48e8834a8b1298087673aac9472c758f2
baskanohn5 <baskanohn5@gmail.com>
2026-05-15 17:48:45 +0300

    Render port binding fix

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const app = require("./src/app");
 require("dotenv").config();
+
+const app = require("./src/app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server ${PORT} portunda çalışıyor`);
 });
